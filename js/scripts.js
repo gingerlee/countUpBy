219d23ffc845e51602507ed15a32d22c6075f259
Cat Janowitz and Ginger Lee Kretschmer <cat@>
2017-10-24 09:42:58 -0700

    make function that successfully counts numbers in desired increments and begin to flesh out html form

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  //
+  // var index = [];
+  // for (var currentNumber = 5; currentNumber <= 30; currentNumber += 5) {
+  //   index.push(currentNumber);
+  // }
+  // alert(index);
+
+//   var output = [];
+// for (var currentNum = 7; currentNum <= 50; currentNum += 7) {
+// 	output.push(currentNum);
+// }
+// alert(output);
+
+
+});
